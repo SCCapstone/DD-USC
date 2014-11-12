37807d2bceb2551e87e73a768f9f7a2884bb5f2b
cjenne18 <jenne@email.sc.edu>
2014-11-12 16:12:01 -0500

    Front Page
    Theres some issue with the simulator or the view controller. I am not
    100% sure but for now it looks good enough.

---
+++ DD-USC-Jenne/DD-USC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DD-USC
+//
+//  Created by Casey Cole on 10/29/14.
+//  Copyright (c) 2014 Casey Cole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
