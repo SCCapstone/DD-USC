bcb901f01635b374a2932a8605e237a534874065
Cole Mendes <mendesc@email.sc.edu>
2014-12-05 14:42:14 -0500

    Fixed events page

---
+++ DD-USC/DD-USC/EventViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventViewController.h
+//  DD-USC
+//
+//  Created by Cole Mendes on 12/5/14.
+//  Copyright (c) 2014 Casey Cole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventViewController : UIViewController
+
+@end
